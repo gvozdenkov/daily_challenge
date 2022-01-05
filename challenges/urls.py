e3915346d647f2a9f4b13af5adbb207cb47cb445
arty <gvozdenkov@gmail.com>
2022-01-06 00:08:42 +0300

    create challenge app logic with inbox html generation

--- challenges/urls.py
+++ challenges/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
+from . import views
+
+
 urlpatterns = [
-    path()
+    path('', views.index),
+    path('<int:month>', views.monthly_challenge_by_number),
+    path('<str:month>', views.monthly_challenge, name="month_challenge")            # отслеживает все переменные url <month> и возвращает соответствующие отображения view views.index
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,45 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseNotFound
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
+monthly_challenges = {
+    "january": "this is jan",
+    "february": "this is feb",
+    "march": "this is march",
+    "april": "this is april",
+}
+
 def index(request):
-    return HttpResponse("YO!")
+    list_item = ""
+    months = list(monthly_challenges.keys())
+    
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month_challenge", args=[month])
+        list_item += f"<li><a href='{month_path}'>{capitalized_month}</a></li>"
+    
+    response_data = f"<ul>{list_item}</ul>"
+    return HttpResponse(response_data)
+
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+        string = f"here is {month} with challenge: {challenge_text}"
+        return HttpResponse(string)
+    except:
+        return HttpResponseNotFound("month not found")
+
+
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+    if month > len(months):
+        return HttpResponseNotFound("wrong month")
+    else:
+        redirect_month = months[month-1]
+        redirect_path = reverse("month_challenge", args=[redirect_month])          # /challenge/<january> по имени возврщает path приложения (модуля), через args - передача аргумента <month>
+        return HttpResponseRedirect(redirect_path)
