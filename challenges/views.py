447730ad7c38d087f669cd21efed524fe7d1e567
arty <gvozdenkov@gmail.com>
2022-01-11 10:47:07 +0300

    finalize dummy proj

--- challenges/views.py
+++ challenges/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
 from django.http import HttpResponseNotFound
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+
 
 # Create your views here.
 
@@ -12,7 +13,14 @@ monthly_challenges = {
     "march": "Try something cool this month niga!",
     "april": "this is april",
     "may": "Make yourself in may yo!",
-    "june": None
+    "june": "relax man",
+    "july": "july go to street",
+    "augest": "something for august",
+    "september": "Take a coffie",
+    "october": "Yey, october here bro, cool!",
+    "november": "uffff, winter is here",
+    "december": "december is a cold month bro"
+
 }
 
 def index(request):
@@ -31,8 +39,7 @@ def monthly_challenge(request, month):
             "indexlink": indexlink
         })
     except:
-        return HttpResponseNotFound("month not found")
-
+        raise Http404()         # ищет именно 404.html файл (файл хранится в директоии, которая указана в настройках проекта Templates
 
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
--- daily_challenge/settings.py
+++ daily_challenge/settings.py
@@ -57,7 +57,10 @@ ROOT_URLCONF = 'daily_challenge.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            # BASE_DIR / "challenges" / "templates",
+            BASE_DIR / "templates"
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,10 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "static"),
+#    os.path.join(BASE_DIR, "static"),
+    BASE_DIR / "static"
 ]
 
 # Default primary key field type
